3a4f7e88b755edd53dd5f17e701f461d14a1a5a7
Mrlijinchao <2116639781@qq.com>
2023-04-05 15:19:24 +0800

    完成了商品列表页面的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		GoodList
+		<view class="goods-list">
+			<view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +12,98 @@
 	export default {
 		data() {
 			return {
+				// 请求参数对象
+				queryObj:{
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				isloading: false
 				
 			};
+		},
+		
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			this.getGoodsList()
+		},
+		
+		methods:{
+			//获取商品列表数据的方法
+			async getGoodsList(cb){
+				//打开节流阀
+				this.isloading = true
+				const {data: res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj)
+				this.isloading = false
+				cb && cb()
+				if(res.meta.status !== 200) return uni.$showMsg()
+				this.goodsList = [...this.goodsList,...res.message.goods]
+				this.total = res.message.total
+			},
+			
+			gotoDetail(goods){
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+				})
+			}
+			
+		},
+		
+		onReachBottom(){
+			if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total)
+			return uni.$showMsg('数据加载完毕')
+			
+			if(this.isloading) return
+			// 让页码自增加一
+			this.queryObj.pagenum++
+			this.getGoodsList()
+		},
+		
+		onPullDownRefresh(){
+			//重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
+			
+			// 重新发起数据请求
+			this.getGoodsList(() => uni.stopPullDownRefresh())
 		}
+		
 	}
 </script>
 
 <style lang="scss">
+.goods-item{
+	display: flex;
+	padding: 10px 5px #f0f0f0;
+	border-bottom: 1px solid;
+	.goods-item-left{
+		margin-right: 5px;
+		.goods-pic{
+			width: 100px;
+			height: 100px;
+			display: block;
+		}
+	}
+	.goods-item-right{
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		.goods-name{
+			font-size: 13px;
+		}
+		.goods-info-box{
+			.goods-price{
+				color: #C00000;
+				font-size: 16px;
+			}
+		}
+	}
+}
 
 </style>
---
+++ uni_modules/my-goods/components/my-goods/my-goods.vue
@@ -0,0 +1,71 @@
+<template>
+	<view>
+		<view class="goods-item">
+		<!-- 左侧的盒子 -->
+		<view class="goods-item-left">
+			<image :src="goods.goods_small_logo || defaultPic"
+			class="goods-pic"></image>
+		</view>
+		<!-- 右侧的盒子 -->
+		<view class="goods-item-right">
+			<!-- 商品的名字 -->
+			<view class="goods-name">{{goods.goods_name}}</view>
+			<view class="goods-info-box">
+				<view class="goods-price">{{goods.goods_price | tofixed}}</view>
+			</view>
+		</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default{
+		props: {
+			goods:{
+				type: Object,
+				default: {}
+			}
+		},
+		data() {
+			return{
+				// 默认的图片
+				defaultPic: 'https://desk-fd.zol-img.com.cn/t_s960x600c5/g4/M06/06/0B/Cg-4zFTv4zmINhM9ABX5yHpH5GYAAVpZgI6vOUAFfng315.jpg',
+				
+			};
+		},
+		filters:{
+			tofixed(num) {
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+<style lang="scss">
+	.goods-item{
+		display: flex;
+		padding: 10px 5px #f0f0f0;
+		border-bottom: 1px solid;
+		.goods-item-left{
+			margin-right: 5px;
+			.goods-pic{
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+		.goods-item-right{
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			.goods-name{
+				font-size: 13px;
+			}
+			.goods-info-box{
+				.goods-price{
+					color: #C00000;
+					font-size: 16px;
+				}
+			}
+		}
+	}
+	
+</style>
